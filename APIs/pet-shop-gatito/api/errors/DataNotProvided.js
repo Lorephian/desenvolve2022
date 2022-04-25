83df98a3ac8fcd59558714fe68d76aaed345a45a
lorephian <guilherme.thomas87@gmail.com>
2022-04-24 23:31:36 -0300

    delete

--- APIs/pet-shop-gatito/api/errors/DataNotProvided.js
+++
@@ -1,9 +0,0 @@
-class DataNotProvided extends Error {
-    constructor () {
-        super('Data was not provided to perform an update')
-        this.name = 'DataNotProvided'
-        this.idError = 2
-    }
-}
-
-module.exports = DataNotProvided
--- APIs/pet-shop-gatito/api/errors/NotFound.js
+++
@@ -1,9 +0,0 @@
-class NotFound extends Error {
-    constructor () {
-        super('Supplier not found')
-        this.name = 'NotFound'
-        this.idError = 0
-    }
-}
-
-module.exports = NotFound
