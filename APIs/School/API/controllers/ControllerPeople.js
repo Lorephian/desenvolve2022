978719f54d164db0338b855f03430d2bbdccf4e9
lorephian <guilherme.thomas87@gmail.com>
2022-04-26 14:59:14 -0300

    added the post, put and delete methods and routes

--- APIs/School/API/controllers/ControllerPeople.js
+++ APIs/School/API/controllers/ControllerPeople.js
@@ -6,9 +6,51 @@ class ControllerPeople {
             const allPeople = await database.People.findAll()
             return res.status(200).json(allPeople)
         } catch (error) {
-            return res.status(500).json(errors.message)
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async catchOnePerson(req, res) {
+        const {id} = req.params
+        try{
+            const onePerson = await database.People.findOne( {where: { id: Number(id)}})
+            return res.status(200).json(onePerson)
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async createPerson(req, res) {
+        const newPerson = req.body
+        try{
+            const newCreatedPerson = await database.People.create(newPerson)
+            return res.status(200).json(newCreatedPerson)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async updatePerson(req, res) {
+        const {id} = req.params
+        const newInfos = req.body
+        try {
+            await database.People.update(newInfos, {where: {id:Number(id)}})
+            const updatedPerson = await database.People.findOne({where: {id: Number(id)}})
+            return res.status(200).json(updatedPerson)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+    static async deletePerson(req, res) {
+        const {id} = req.params
+        try {
+            await database.People.destroy({where: {id: Number(id)}})
+            return res.status(200).json({message: `id ${id} deleted`})
+        } catch (error) {
+            return res.status(500).json(error.message)
         }
     }
 }
+
+
 
 module.exports = ControllerPeople
--- APIs/School/API/routes/routePeople.js
+++ APIs/School/API/routes/routePeople.js
@@ -4,5 +4,9 @@ const ControllerPeople = require('../controllers/ControllerPeople')
 const router = Router()
 
 router.get('/people', ControllerPeople.catchAllPeople)
+router.get('/people/:id', ControllerPeople.catchOnePerson)
+router.post('/people', ControllerPeople.createPerson)
+router.put('/people/:id', ControllerPeople.updatePerson)
+router.delete('/people/:id', ControllerPeople.deletePerson)
 
 module.exports = router
