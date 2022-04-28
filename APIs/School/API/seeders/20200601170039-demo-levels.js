b29e4d414b0c8dab2a1e23efcdae045239d0c48a
lorephian <guilherme.thomas87@gmail.com>
2022-04-28 17:46:35 -0300

    seeded the database

---
+++ APIs/School/API/seeders/20200601170039-demo-levels.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert('Levels', [
+			{
+				desc_level: 'basic',
+				createdAt: new Date(),
+				updatedAt: new Date()			
+			},
+			{
+				desc_level: 'intermediate',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				desc_level: 'advanced',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			} 
+	], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete('Levels', null, {})
+  }
+}
